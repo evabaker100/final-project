9e39340f35650634bba5549633b1b4079810c031
SCSFMAC5 <scsfmac5@Admins-MacBook-Air.local>
2018-05-24 18:08:11 -0700

    finalchanges

---
+++ src/commons/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+   <p className="navbar-brand" href="#">Navbar</p>
+   </nav>
+)
+
+export default Header
